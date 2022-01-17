912774ed7df378e211cab7a0aa90765e16ef2dda
gearnode <bryan.frimin@exograd.com>
2022-01-17 20:55:17 +0100

    Add inline references support

--- src/openapi_v3_spec.erl
+++ src/openapi_v3_spec.erl
@@ -38,13 +38,50 @@ read(Data) ->
 
 -spec read_value(json:value()) ->
         {ok, openapi_v3:specification()} | {error, openapi_v3:error_reason()}.
-read_value(Value) ->
+read_value(Value0) ->
+  Value = inline_references(Value0),
   Options = #{type_map => openapi_jsv:type_map(),
               unknown_member_handling => keep,
               format_value_errors => true},
   case jsv:validate(Value, {ref, openapi_v3, specification}, Options) of
-    {ok, Spec} ->
-      {ok, Spec};
+    {ok, Data} ->
+      {ok, Data};
     {error, Errors} ->
       {error, {invalid_specification, Errors}}
   end.
+
+-spec inline_references(map()) -> map().
+inline_references(Data) ->
+  inline_references(maps:next(maps:iterator(Data)), Data, #{}).
+
+-spec inline_references(none | {term(), term(), maps:iterator()}, map(), map()) -> map().
+inline_references(none, _, Acc) ->
+  Acc;
+inline_references({Key, Value0, Iterator}, Spec, Acc) when is_map(Value0) ->
+  Value1 = maybe_resolve_ref(Spec, Value0),
+  Value = inline_references(maps:next(maps:iterator(Value1)), Spec, #{}),
+  inline_references(maps:next(Iterator), Spec, Acc#{Key => Value});
+inline_references({Key, Value0, Iterator}, Spec, Acc) when is_list(Value0) ->
+  F = fun
+        (Element) when is_map(Element) ->
+          inline_references(maps:next(maps:iterator(Element)), Spec, #{});
+        (Element) ->
+          Element
+      end,
+  Value = lists:map(F, Value0),
+  inline_references(maps:next(Iterator), Spec, Acc#{Key => Value});
+inline_references({Key, Value, Iterator}, Spec, Acc) ->
+  inline_references(maps:next(Iterator), Spec, Acc#{Key => Value}).
+
+%% TODO: manage recursive reference.
+maybe_resolve_ref(Spec, #{<<"$ref">> := <<"#", Ref/binary>>}) ->
+  case json_pointer:find(Ref, Spec) of
+    {ok, #{<<"$ref">> := _} = Term} ->
+      maybe_resolve_ref(Spec, Term);
+    {ok, Term} ->
+      Term;
+    {error, Reason} ->
+      throw({error, {invalid_reference, Reason}})
+  end;
+maybe_resolve_ref(_, Term) ->
+  Term.
