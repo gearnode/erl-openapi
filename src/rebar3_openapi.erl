ffa35f6f80f17220e139123aeea770e8979901c7
gearnode <bryan.frimin@exograd.com>
2022-02-18 17:04:29 +0100

    Bootstrap rebar3 plugin

---
+++ src/rebar3_openapi.erl
@@ -0,0 +1,29 @@
+%% Copyright (c) 2021-2022 Exograd SAS.
+%%
+%% Permission to use, copy, modify, and/or distribute this software for any
+%% purpose with or without fee is hereby granted, provided that the above
+%% copyright notice and this permission notice appear in all copies.
+%%
+%% THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+%% WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+%% MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+%% SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+%% WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+%% ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+%% IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+-module(rebar3_openapi).
+
+-export([init/1, do/1, format_error/1]).
+
+-spec init(rebar_state:t()) -> {ok, rebar_state:t()}.
+init(State0) ->
+  {ok, State1} = rebar3_openapi_generate:init(State0),
+  {ok, State1}.
+
+do(State) ->
+  {ok, State}.
+
+-spec format_error(any()) -> iolist().
+format_error(Reason) ->
+    io_lib:format("~p", [Reason]).
---
+++ src/rebar_openapi_generate.erl
@@ -0,0 +1,42 @@
+%% Copyright (c) 2021-2022 Exograd SAS.
+%%
+%% Permission to use, copy, modify, and/or distribute this software for any
+%% purpose with or without fee is hereby granted, provided that the above
+%% copyright notice and this permission notice appear in all copies.
+%%
+%% THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+%% WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+%% MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+%% SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+%% WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+%% ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+%% IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+-module(rebar_openapi_generate).
+
+-export([init/1, do/1, format_error/1]).
+
+-spec init(rebar_state:t()) -> {ok, rebar_state:t()}.
+init(State) ->
+  Provider =
+    providers:create([{name, generate},
+                      {namespace, openapi},
+                      {module, ?MODULE},
+                      {bare, true},
+                      {deps, [{default, app_discovery}]},
+                      {opts, []},
+                      {example,
+                       "rebar3 openapi generate"},
+                      {short_desc,
+                       "Generate files based on OpenAPI v3 specification."},
+                      {desc,
+                       "Generate files based on OpenAPI v3 specification."}]),
+  {ok, rebar_state:add_provider(State, Provider)}.
+
+-spec do(rebar_state:t()) -> {ok, rebar_state:t()} | {error, string()}.
+do(State) ->
+  {ok, State}.
+
+-spec format_error(any()) -> iolist().
+format_error(Reason) ->
+  io_lib:format("~p", [Reason]).
