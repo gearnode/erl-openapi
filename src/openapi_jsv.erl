e375768945d76f8feb98cd20263185cd5a00721b
gearnode <bryan.frimin@exograd.com>
2022-02-21 09:41:23 +0100

    Fix allow minor version

--- src/openapi_jsv.erl
+++ src/openapi_jsv.erl
@@ -57,7 +57,7 @@ catalog() ->
 specification_definition() ->
   {object,
    #{members =>
-       #{openapi => {string, #{values => ['3.0.0', '3.0.1']}},
+       #{openapi => {string, #{values => ['3.0.0', '3.0.1', '3.0.2', '3.0.3']}},
          info => {ref, info},
          servers => {array, #{element => {ref, server}}},
          paths => {object, #{value => {ref, path}}},
