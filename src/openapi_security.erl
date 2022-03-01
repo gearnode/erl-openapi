c03a7cb29e5a326132f74b355c4b11f1c2671fe6
gearnode <bryan.frimin@exograd.com>
2022-03-01 14:46:41 +0100

    Add openapi security module

---
+++ src/openapi_security.erl
@@ -0,0 +1,25 @@
+%% Copyright (c) 2022 Exograd SAS.
+%%
+%% Permission to use, copy, modify, and/or distribute this software for any
+%% purpose with or without fee is hereby granted, provided that the above
+%% copyright notice and this permission notice appear in all copies.
+%%
+%% THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+%% WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+%% MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+%% SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+%% WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+%% ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+%% IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+-module(openapi_security).
+
+-export([is_optional/1]).
+
+-spec is_optional([openapi:security_requirement()]) -> boolean().
+is_optional([]) ->
+  false;
+is_optional([#{} | _]) ->
+  true;
+is_optional([_ | T]) ->
+  is_optional(T).
